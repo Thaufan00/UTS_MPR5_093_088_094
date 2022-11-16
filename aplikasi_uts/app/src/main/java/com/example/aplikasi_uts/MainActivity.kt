24c3b1dbebadacb055c1ccb90913f13a0667d8fd
farhan093 <hayasai.121@gmail.com>
2022-11-16 18:57:49 +0700

    progress tampilan layar

---
+++ aplikasi_uts/app/src/main/java/com/example/aplikasi_uts/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.aplikasi_uts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
